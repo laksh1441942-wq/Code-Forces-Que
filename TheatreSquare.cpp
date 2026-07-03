7b384a4e539b6aaff7bb94bc27681fbeed65adb3
Laksh Sharma <laksh1441942@gmail.com>
2026-07-03 23:46:06 +0530

    Solve 1A - Theatre Square

---
+++ TheatreSquare.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    long long n, m, a;
+    cin>>n>>m>>a;
+    long long ans = ((n+a-1)/a)*((m+a-1)/a);
+    cout<<ans;
+    return 0;
+}
