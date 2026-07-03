3c74ad5bdaf4a58a630b7f0bd12a332488591246
Laksh Sharma <laksh1441942@gmail.com>
2026-07-03 21:41:13 +0530

    changes

--- WayTooLongWords.cpp
+++ WayTooLongWords.cpp
@@ -2,15 +2,13 @@
 #include<string>
 using namespace std;
 int main(){
-    string str;
-    cin>>str;
-    if(str.length() <= 10){
-        cout<<str<<endl;
-        return 0;
+    int n;
+    cin>>n;
+    string s;
+    while(n--){
+        cin>>s;
+        if(s.length() > 10) cout<<s[0]<<s.length()-2<<s[s.length()-1]<<endl;
+        else cout<<s<<endl;
     }
-    int cnt = 0;
-    for(char it : str ){
-        cnt++;
-    }
-    cout<<str.substr(0,1)<<cnt-2<<str.substr(cnt-1,1)<<endl;
+   return 0;
 }
