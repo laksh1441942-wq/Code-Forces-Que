4da3aed0fa3df712453523c178475a087b9f6101
Laksh Sharma <laksh1441942@gmail.com>
2026-07-12 17:27:50 +0530

    Solve 110A - Nearly Lucky Number

---
+++ NearlyLuckyNumber.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    long long n;
+    cin>>n;
+    long long count = 0;
+    while(n>0){
+        long long digit = n%10;
+        if(digit==4 || digit==7){
+            count++;
+        }
+        n/=10;
+    }
+    if(count==4 || count==7){
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+    return 0;
+}
